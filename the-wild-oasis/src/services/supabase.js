56158751f3a5a0fbe61a5a7e8c83368d48f5272e
bishoy <bishoy.saberibrahim@gmail.com>
2024-05-21 23:29:07 +0300

    final commit

--- the-wild-oasis/src/services/supabase.js
+++ the-wild-oasis/src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://utojoxjrluucvjenakto.supabase.co";
-const supabaseKey =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0b2pveGpybHV1Y3ZqZW5ha3RvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NzQ5MjAsImV4cCI6MjAyODQ1MDkyMH0.snyT9byHgJ0Y5HSvY1xmoyG8mz_2T1CuC0J4PpQEjzc";
+export const supabaseUrl = "";
+const supabaseKey = "";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
